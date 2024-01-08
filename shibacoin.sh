fb4cd8e8b487d6d42c4407267075ce32cbf9e34e
legalspeedinves <52072678+legalspeedinves@users.noreply.github.com>
2024-01-08 19:48:58 +0700

    Create shibacoin.sh

---
+++ shibacoin.sh
@@ -0,0 +1,4 @@
+# shibacoin
+apt update
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.4.4/SRBMiner-Multi-2-4-4-Linux.tar.xz && tar -xvf SRBMiner-Multi-2-4-4-Linux.tar.xz && cd SRBMiner-Multi-2-4-4 && miner start 
+./SRBMiner-MULTI --algorithm randomx --pool rx.unmineable.com:3333 --wallet SHIB:0xbb52b64d5fb09e2aedd49cb405dcfb5d077498fb.OyesMiner --threads=4
